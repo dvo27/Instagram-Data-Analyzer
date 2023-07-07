828000b9d9ea7a09ac9a183c24a5bf5cccf01cec
Daniel Vo <danielnv@uci.edu>
2023-07-07 02:01:03 -0700

    Created message_data function to read through a conversation JSON and print a df with the 5 most common words

--- read_JSON.py
+++ read_JSON.py
@@ -19,14 +19,15 @@ def message_data():
             messages_dict = message_json['messages']
             df = pd.DataFrame.from_dict(messages_dict)
 
-            # Skips action messages
+            # Creates new df skipping action messages, returning the messages while skipping over all NotANumber values
             content_df = df.loc[df['content'].str.contains("sent an attachment.|shared a story.|Liked a message") == False].copy()
             content_column = content_df['content']  # Returns the content column of the newly copied dataframe
             content_df.dropna(inplace=True)  # Skips all NotANumber values
 
             # Creates list of tuples with five most common words + counts and returns
-            five_most_common_words = Counter(" ".join(content_column).split()).most_common(5)
-            print(five_most_common_words)
+            common_word_counter = Counter(" ".join(content_column).split()).most_common(5)
+            common_word_df = pd.DataFrame(common_word_counter, columns=['Word', 'Count'])
+            print(common_word_df)
     except FileNotFoundError:
         print('ERROR: The given directory does not exist or is not a valid path')
 
