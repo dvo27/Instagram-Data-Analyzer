3afc79744b439a71b995f4ee12bbb042df8a3c00
Daniel Vo <danielnv@uci.edu>
2023-07-08 03:26:14 -0700

    Created the start of a main menu gui with error handling

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import sys
+
 import read_JSON as read
 
+
+def main():
+    valid_options = ['1', 'Q', 'q']
+    menu_choice = input('Please choose an option below!:'
+                        '\n[1] : Get DMs With Specific User Data\n'
+                        '[Q] : Quit Program\n'
+                        '----------------------------------------\n')
+    if menu_choice not in valid_options:
+        print('\nInvalid option! Please try again.\n')
+        main()
+    else:
+        if menu_choice == '1':
+            read.message_data()
+        elif menu_choice == 'Q' or 'q':
+            print('\nEnding program...Goodbye!')
+            sys.exit()
+
+
 if __name__ == '__main__':
-    read.message_data()
+    main()
