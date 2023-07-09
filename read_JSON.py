3862b9303a5dfbc607ef33edf912072171bb6fea
Daniel Vo <danielnv@uci.edu>
2023-07-09 01:33:54 -0700

    Created get_first_five_messages() including supplemental functions to implement getting first five messages in a conversation including correct formatting of timestamps and utf-8 encoding of messages

--- read_JSON.py
+++ read_JSON.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from collections import Counter
 import pandas as pd
 
@@ -9,7 +10,7 @@ def create_msg_df(input_path):
     :param input_path: A Path object of a path to a message JSON file
     :return: A dataframe containing information in the message JSON file
     """
-    with open(input_path) as message_file:
+    with open(input_path, encoding='UTF-8') as message_file:
         message_json = json.load(message_file)
     messages_dict = message_json['messages']
     df = pd.DataFrame.from_dict(messages_dict)
@@ -24,7 +25,8 @@ def filter_msg_content(df):
     """
     # Creates a copy of the df removing the following phrases within the content column
     content_df = df.loc[
-        df['content'].str.contains("sent an attachment.|shared a story.|Liked a message|Reacted|to your message") == False].copy()
+        df['content'].str.contains(
+            "sent an attachment.|shared a story.|Liked a message|Reacted|to your message") == False].copy()
     content_df['content'].dropna(inplace=True)  # Skips all NotANumber values
     content_column = content_df['content']  # Returns the content column of the newly copied dataframe
     return content_column
@@ -50,14 +52,57 @@ def get_message_df_length(df):
     :return: F-string containing the amount of messages in a conversation excluding action statements
     """
     content_column = filter_msg_content(df)
-    return f'The number of messages in the conversation: \n{content_column.size}'
+    return f'\nNumber Of Messages In The Conversation: \n{content_column.size}'
+
+
+def decode_messages(messages):
+    """
+    Decode messages from their original encoding to UTF-8.
+    :param messages: Series containing the messages to be decoded
+    :return: Decoded messages
+    """
+    decoded_messages = messages.apply(lambda message: message.encode('latin-1').decode('utf-8'))
+    return decoded_messages
+
+
+def format_timestamps(timestamps):
+    """
+    Format timestamps from milliseconds to a proper date format.
+    :param timestamps: Series containing the timestamps to be formatted
+    :return: Formatted timestamps
+    """
+    formatted_timestamps = timestamps.apply(lambda time: datetime.datetime.fromtimestamp(time / 1000)
+                                            .strftime('%m-%d-%Y %H:%M'))
+    return formatted_timestamps
 
 
 def get_first_five_messages(df):
-    content_df = df.loc[
+    """
+    Gets the first five messages in a DM conversation and shows each sender and the timestamp
+    along with their message.
+    :param df: A DataFrame object containing the data from the messages with a user
+    :return: F-string containing a DataFrame string of the first five messages
+    """
+    filtered_df = df.loc[
         df['content'].str.contains(
-            "sent an attachment.|shared a story.|Liked a message|Reacted|to your message") == False].copy()
-    content_df['content'].dropna(inplace=True)
+            "sent an attachment.|shared a story.|Liked a message|Reacted|to your message|liked a message") == False].copy()
+    filtered_df['content'].dropna(inplace=True)
+
+    # Get first five messages and reverse the order
+    reversed_filtered_df_head = filtered_df.tail(5)[['sender_name', 'timestamp_ms', 'content']].iloc[::-1]
+
+    # Decode messages
+    reversed_filtered_df_head['content'] = decode_messages(reversed_filtered_df_head['content'])
+
+    # Format timestamps
+    reversed_filtered_df_head['timestamp_ms'] = format_timestamps(reversed_filtered_df_head['timestamp_ms'])
+
+    # Renaming columns to fit new changes
+    reversed_filtered_df_head.rename(columns={
+        'sender_name': 'sender_name', 'timestamp_ms': 'timestamp', 'content': 'message'}, inplace=True)
+    reversed_filtered_df_head = reversed_filtered_df_head.to_string(index=False, justify='left')
+
+    return f'\nYour First Five Messages: \n{reversed_filtered_df_head}\n'
 
 
 def message_data():
@@ -66,9 +111,8 @@ def message_data():
     try:
         file_path = input('Please input path to message data file, ending in .json: ')
         df = create_msg_df(file_path)
-
         print(five_most_common_words(df))
-        print()
         print(get_message_df_length(df))
+        print(get_first_five_messages(df))
     except FileNotFoundError:
         print('\nERROR: The given directory does not exist or is not a valid path')
